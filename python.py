3465d3fcf8cc7cafbfb0134f576d672eff20a312
choijinhooo <chlwlsgh78@naver.com>
2018-12-18 15:43:49 +0900

    fixed

--- python.py
+++ python.py
@@ -1,9 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://finance.naver.com/sise/'
-
-rp = requests.get(url).text
-soup = BeautifulSoup(rp,'html.parser')
-select = soup.select_one("#KOSPI_now")
-print(select.text)
+print("hello")
+print("hihihihi")
